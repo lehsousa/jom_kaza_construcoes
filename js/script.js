fbbd19f5c81fbfb081b25fbe6c6ef4751ce9d8ed
Leandro Sousa <leandrosousa.work@gmail.com>
2022-05-30 20:29:25 -0300

    carrosel

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var swiper = new Swiper('.mySwiper', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    }
+})
