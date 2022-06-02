77da1b6f8767955d32397941a6cc7129b6ca496a
Leandro Sousa <leandrosousa.work@gmail.com>
2022-06-01 23:01:32 -0300

    Aplicado responsividade

--- js/script.js
+++ js/script.js
@@ -17,3 +17,42 @@ var swiper = new Swiper('.mySwiper', {
         prevEl: '.swiper-button-prev'
     }
 })
+window.addEventListener('resize', function() {
+    //var altura = window.innerHeight;
+    var largura = window.innerWidth
+
+    if (largura < 615) {
+        swiper.slidesPerview = 0
+        swiper.slidesPerGroup = 0
+        console.log(swiper)
+    }
+})
+
+const navbar = document.querySelector('.navbar')
+const menuMobile = document.querySelector('.menu-mobile')
+const menuItens = document.querySelectorAll('.menu-itens')
+
+function unloadScrollBars() {
+    document.documentElement.style.overflow = 'hidden'
+    document.body.scroll = 'no' // IE
+}
+
+function reloadScrollBars() {
+    document.documentElement.style.overflow = 'auto'
+    document.body.scroll = 'yes' // IE
+}
+
+menuMobile.addEventListener('click', () => {
+    if (navbar.classList.toggle('active')) {
+        unloadScrollBars()
+    } else {
+        reloadScrollBars()
+    }
+})
+
+menuItens.forEach(e => {
+    e.addEventListener('click', () => {
+        navbar.classList.toggle('active')
+        09
+    })
+})
